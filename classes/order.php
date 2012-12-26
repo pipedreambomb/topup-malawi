31c3b9d7bb01ce5482aab13673be1452550482bb
George Nixon <g@georgenixon.co.uk>
2012-12-26 20:09:08 +0200

    Order can build itself from target and list of possible topups.

--- classes/order.php
+++ classes/order.php
@@ -3,17 +3,44 @@
 class Order {
 
 	public $topups;
-	public $targetSum, $actualSum, $telco;
+	public $targetSum, $telco;
+	private $database;
 
-	function __construct($telco, $targetSum) {
+	function __construct($telco, $targetSum, $database = null) {
 
 		$this->telco = $telco;
 		$this->targetSum = $targetSum;
-		$this->actualSum = $targetSum;
+		$this->topups = array();
+		if($database == null) {
+			$database = new Database();
+		}
+		$this->database = $database;
 	}
 
+	/* 
+	 * build() - builds the list of topups needed in order to meet the target sum,
+	 * starting with the biggest ones first so as to have as few as possible
+	 */
 	function build() {
-		$denoms = Database::getDenominations($this->telco, "DESC");
-		$this->topups = array(1000, 1000);
+		$denoms = $this->database->getDenominations($this->telco, "DESC");
+		while($this->targetSum > $this->sum()
+			&& count($denoms) > 0) {
+
+			if($denoms[0]['amount'] <= $this->targetSum - $this->sum()) {
+				array_push($this->topups, $denoms[0]['amount']);
+			} else {
+				array_shift($denoms);
+			}	
+		}
+	}
+
+	// Add up topups
+	// @returns sum of all topups selected
+	function sum() {
+		$sum = 0;
+		foreach($this->topups as $topup) {
+			$sum += $topup;
+		}
+		return $sum;
 	}
 }
--- tests/order_test.php
+++ tests/order_test.php
@@ -4,13 +4,26 @@ class TestOfOrders extends UnitTestCase {
 		parent::__construct("Orders Test Case");
 	}
 
+	function setUp() {
+		Mock::generate('Database');
+		$this->mockDatabase = new MockDatabase();
+		$this->mockDatabase->returns("getTelcos", array(array("name" => "Airtel"), array("name" => "TNM")));
+
+		$mockDenomsAsc = array(0 => array("amount" => 1000, "nice_amount" => "1,000"), 1 => array("amount" => 5000, "nice_amount" => "5,000"));
+		$this->mockDatabase->returns("getDenominations", $mockDenomsAsc, array("Airtel"));
+
+		$mockDenomsDesc = array(0 =>array("amount" => 5000, "nice_amount" => "5,000") , 1 => array("amount" => 1000, "nice_amount" => "1,000"));
+		$this->mockDatabase->returns("getDenominations", $mockDenomsDesc, array("Airtel", "DESC"));
+	}
+
 	function testBuildOrderFromTarget() {
 		$target = 2000;
-		$order = new Order("Airtel", $target);
+		$order = new Order("Airtel", $target, $this->mockDatabase);
 		$order->build();
+		$this->assertEqual(count($order->topups), 2, "Two topups used");
 		$this->assertEqual($order->topups[0], 1000, "First topup is 1000mkw");
 		$this->assertEqual($order->topups[1], 1000);
-		$this->assertEqual($order->actualSum, $target, "Order total matches target");
+		$this->assertEqual($order->sum(), $target, "Order total matches target");
 	}
 }
 ?>
