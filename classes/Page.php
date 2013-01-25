fcea440bb0bb786fe0b443df6fb3b726395c953b
George Nixon <g@georgenixon.co.uk>
2013-01-25 20:29:45 +0000

    Tests may have been passing, but all links were broken eg css. Fixed.

--- classes/Page.php
+++ classes/Page.php
@@ -12,25 +12,25 @@ public function render() {
 <html lang="en">
 	<head>
 		<title>Buy Topups</title>
-		<link rel="stylesheet" href="../css/style.css"/>
+		<link rel="stylesheet" href="css/style.css"/>
 		<link href='http://fonts.googleapis.com/css?family=Grand+Hotel|Telex' rel='stylesheet' type='text/css'>
 
 		<!-- Bootstrap -->
-		<link href="../css/bootstrap.css" rel="stylesheet" media="screen">
+		<link href="css/bootstrap.css" rel="stylesheet" media="screen">
 
 		<!-- JavaScript scripts -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script src="../js/bootstrap.js"></script>
+		<script src="js/bootstrap.js"></script>
 	</head>
 	<body>
 		<div id="mainwrapper" class="container">
 			<div id=top class="masthead">
 				<ul class="nav nav-pills pull-right">
-				  <li id="HomeButton"><a href="../home.php">Home</a></li>
-				  <li><a href="../about.php">About</a></li>
-				  <li><a href="../support.php">Support</a></li>
+				  <li id="HomeButton"><a href="home.php">Home</a></li>
+				  <li><a href="about.php">About</a></li>
+				  <li><a href="support.php">Support</a></li>
 				</ul>
-				<a href="../home.php"><img src="../img/topup.png"></img></a>
+				<a href="home.php"><img src="img/topup.png"></img></a>
 			</div>
 			<div id="middle" class="">
 				<?php echo $this->content()?>
