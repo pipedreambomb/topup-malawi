cd86d8c7a94717ec7186475d86907d1b0ee07c3f
George Nixon <g@georgenixon.co.uk>
2013-03-20 17:03:47 +0000

    Added Apache license info.

--- db/sample_db_cfg.php
+++ db/sample_db_cfg.php
@@ -1,3 +1,18 @@
+<?php /*
+* Copyright 2013 George Nixon
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/ ?>
 <?php
 // GN - This is a sample file, I am not putting my actual details in the public
 // source code repo for obvious reasons!
