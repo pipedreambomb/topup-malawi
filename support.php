05df58604e5db453e2de6b50166f1930a9569a42
George Nixon <g@georgenixon.co.uk>
2013-03-20 16:20:59 +0000

    Added (no) support page.

--- support.php
+++ support.php
@@ -4,6 +4,9 @@ require_once "classes/Page.php";
 class Support extends Page {
 
 	protected function content(){
+		?>
+		<p>Sorry, this site is a demo, please see <a href="about.php">the About page</a> for more info and contact details of the developer.</p>
+		<?php
 	}
 
 }
