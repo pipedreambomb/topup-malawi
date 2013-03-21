7df6933c434102f7d189a0145a978e190dd588a3
George Nixon <g@georgenixon.co.uk>
2013-03-21 12:17:05 +0000

    Added Google Analytics tag to template

--- classes/Page.php
+++ classes/Page.php
@@ -38,6 +38,18 @@ public function render() {
 		<script src="js/lib/bootstrap.min.js"></script>
 	</head>
 	<body>
+		<script type="text/javascript">
+			// Google Analytics
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-4804110-11']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			 })();
+		</script>
 		<div id="mainwrapper" class="container">
 			<div id=top class="masthead">
 				<ul class="nav nav-pills pull-right">
